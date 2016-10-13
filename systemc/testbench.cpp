a17afcdf0280419ea0e5afb15fb47f21cd4d7036
Dmitry Khalansky <rouanth@gmail.com>
2016-10-13 22:53:19 +0300

    Written some initial tests for SPI master
    They compile and they start, and even though SPI master doesn't properly
    work yet, it's now only a matter of tedious debugging.

--- systemc/testbench.cpp
+++ systemc/testbench.cpp
@@ -7,9 +7,50 @@
 
 #include "systemc.h"
 #include "spi.h"
+#include "test_spi.h"
 
 int sc_main(int ac, char *av[])
 {
-	return 0;
+    sc_signal<sc_int<8> > data_in, data_out;
+    sc_signal<bool> miso, sclk, ss, mosi, rst, enable;
+
+    data_in.write(0xBE);
+    sc_clock clk ("ID", 10, SC_NS, 0.5, 10, SC_NS, true);
+
+    spi_master spi ("SPI_master");
+    spi.data_in(data_in);
+    spi.data_out(data_out);
+    spi.clk(clk);
+    spi.rst(rst);
+    spi.enable(enable);
+    spi.miso(miso);
+    spi.sclk(sclk);
+    spi.ss(ss);
+    spi.mosi(mosi);
+
+    test_slave test ("SPI_slave");
+    test.clk(sclk);
+    test.mosi(mosi);
+    test.miso(miso);
+    test.ss(ss);
+    test.rst(rst);
+    test.enable(enable);
+    test.data_in(data_in);
+
+    sc_trace_file *tf;
+    tf = sc_create_vcd_trace_file("waves");
+    sc_trace(tf, clk, "clk");
+    sc_trace(tf, rst, "rst");
+    sc_trace(tf, sclk, "sclk");
+    sc_trace(tf, ss, "SS");
+    sc_trace(tf, mosi, "MOSI");
+    sc_trace(tf, miso, "MISO");
+    sc_trace(tf, enable, "enable");
+    sc_trace(tf, data_in, "data_in");
+    sc_trace(tf, data_out, "data_out");
+
+    sc_start();
+
+    return 0;
 }
 
