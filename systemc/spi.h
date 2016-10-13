1053f76502c5ab9d61ad846b5cae7aa037a63ada
Dmitry Khalansky <rouanth@gmail.com>
2016-10-13 21:33:41 +0300

    Added SystemC definitions for SPI master mode 3
    Header files for SPI master have been made, it should work now. The
    project doesn't build yet, and no testing has been implemented, but at
    least it is correct in terms of C++.

--- systemc/spi.h
+++ systemc/spi.h
@@ -1,23 +1,182 @@
 #include "systemc.h"
 
+/* A module to halve the frequency of a clock. */
+SC_MODULE(halved_clk)
+{
+sc_in<bool> clk;
+sc_out<bool> hclk;
+
+void invert() {
+    hclk = !hclk;
+}
+
+SC_CTOR(halved_clk) {
+    SC_METHOD(invert);
+    sensitive_pos << clk;
+}
+
+};
+
+/* A module to reduce the frequency of a clock to a quarter of the original. */
 SC_MODULE(quarted_clk)
 {
 sc_in<bool> clk;
 sc_out<bool> qclk;
 
-sc_signal<bool> sc_tmp;
+halved_clk c1, c2; 
+sc_signal<bool> tmp;
 
-void quarter() {
-    if (sc_tmp) {
-        qclk = ~qclk;
-    }
-    sc_tmp = ~sc_tmp;
+SC_CTOR(quarted_clk): c1("C1"), c2("C2") {
+    c1.clk(clk);
+    c1.hclk(tmp);
+
+    c2.clk(tmp);
+    c2.hclk(qclk);
 }
 
-SC_CTOR(quarted_clk) {
-    SC_METHOD(quarter);
+};
+
+/* The part of SPI mode 3 that's responsible for receiving. */
+SC_MODULE(spi_rx)
+{
+sc_out<sc_int<8> > data_out;
+sc_in<sc_int<3> > ctr;
+sc_in<bool> clk, in, rst;
+
+void rx() {
+    if (rst) {
+        data_out.write(0);
+    } else {
+        sc_int<3> data_out_tmp = data_out.read();
+        data_out_tmp[ctr.read()] = in.read();
+        data_out.write(data_out_tmp);
+    }
+}
+
+SC_CTOR(spi_rx) {
+    SC_METHOD(rx);
     sensitive_pos << clk;
 }
+};
 
+/* The part of SPI mode 3 that's responsible for transmission. */
+SC_MODULE(spi_tx)
+{
+sc_in<sc_int<8> > data_in;
+sc_in<sc_int<3> > ctr;
+sc_in<bool> clk;
+sc_out<bool> out;
+
+void tx() {
+    out = data_in.read()[ctr.read()];
+}
+
+SC_CTOR(spi_tx) {
+    SC_METHOD(tx);
+    sensitive_pos << clk;
+}
+};
+
+/* Main loop thread for SPI master with mode 3. */
+SC_MODULE(spi_master_loop)
+{
+sc_in<bool> clk, rst, enable;
+sc_in<sc_int<8> > input;
+sc_out<bool> ss, do_tx, do_rx;
+sc_out<sc_int<3> > ctr;
+sc_out<sc_int<8> > cache;
+
+void state() {
+    bool enabled = false;
+    bool trailing = false;
+    sc_int<3> ctr_temp = 0;
+    ss.write(true);
+
+    while (true) {
+        wait();
+        do_tx = false;
+        do_rx = false;
+        if (clk) {
+            if (rst) {
+                ss.write(true);
+                cache = 0;
+                enabled = false;
+                trailing = false;
+                ctr_temp = 0;
+            } else {
+                if (trailing) {
+                    trailing = false;
+                    ss.write(true);
+                } else if (enabled && (ctr_temp == 7)) {
+                    do_tx = true;
+                    trailing = true;
+                } else if (enable && !enabled) {
+                    ss.write(false);
+                    ctr_temp = 0;
+                    cache = input;
+                    enabled = true;
+                } else if (enabled) {
+                    do_tx = true;
+                }
+                ctr.write(ctr);
+            }
+        } else {
+            if (enabled) {
+                do_rx = true;
+            }
+        }
+    }
+}
+
+SC_CTOR(spi_master_loop) {
+    SC_THREAD(state);
+    sensitive << clk;
+}
+};
+
+/* SPI master with mode 3. */
+SC_MODULE(spi_master)
+{
+sc_out<sc_int<8> > data_in;
+sc_out<sc_int<8> > data_out;
+sc_in<bool> clk, rst, enable, miso;
+sc_out<bool> sclk, ss, mosi;
+
+spi_rx rx;
+spi_tx tx;
+quarted_clk qclk;
+spi_master_loop lp;
+
+sc_signal<sc_int<3> > ctr;
+sc_signal<sc_int<8> > cache;
+sc_signal<bool> do_tx, do_rx;
+
+SC_CTOR(spi_master):
+    rx("RX"), tx("TX"), qclk("QCLK"), lp("LP"),
+    do_tx("do_tx"), do_rx("do_rx") {
+        qclk.clk(clk);
+        qclk.qclk(sclk);
+
+        lp.clk(sclk);
+        lp.rst(rst);
+        lp.enable(enable);
+        lp.input(data_in);
+        lp.ss(ss);
+        lp.do_tx(do_tx);
+        lp.do_rx(do_rx);
+        lp.ctr(ctr);
+        lp.cache(cache);
+
+        rx.data_out(data_out);
+        rx.ctr(ctr);
+        rx.clk(do_rx);
+        rx.in(miso);
+        rx.rst(rst);
+
+        tx.data_in(data_in);
+        tx.ctr(ctr);
+        tx.clk(do_tx);
+        tx.out(mosi);
+    }
 };
 
---
+++ systemc/testbench.cpp
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ 
+  main.cpp -- This is the top level file instantiating the modules and
+              binding ports to signals.
+ 
+ *****************************************************************************/
+
+#include "systemc.h"
+#include "spi.h"
+
+int sc_main(int ac, char *av[])
+{
+	return 0;
+}
+
