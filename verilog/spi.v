81fcbb1e576ea29321431084a2079f40f987f2be
Dmitry Khalansky <rouanth@gmail.com>
2016-10-13 19:30:09 +0300

    Replaced 3-spaced tabs with spaces.
    Because how insane must you be to have tabs of width 3?

--- verilog/spi.v
+++ verilog/spi.v
@@ -3,13 +3,13 @@
 module spi(
     input clk,                 // Тактовый сигнал с процессора
     input miso,                // Master in slave out
-	 input[7:0] data_in,        // Данные для передачи от процессора
-	 input ready_send,          // Данные положены в data-in и готовы к отправке
-	 input rst,
+    input[7:0] data_in,        // Данные для передачи от процессора
+    input ready_send,          // Данные положены в data-in и готовы к отправке
+    input rst,
     output reg mosi,           // Master out slave in
     output reg sclk = 0,       // Тактовый сигнал к рабам
     output reg ss,             // Slave select
-	 output reg[7:0] data_out   // Принятые данные от раба
+    output reg[7:0] data_out   // Принятые данные от раба
     );
 
 localparam IDLE = 0;
@@ -21,46 +21,46 @@ reg[7:0] data_in_reg;
 reg      clk_tmp = 0;
 
 always @(posedge clk) begin
-	clk_tmp <= !clk_tmp;
-	if (clk_tmp) begin
-		sclk <= !sclk;
-	end
+   clk_tmp <= !clk_tmp;
+   if (clk_tmp) begin
+      sclk <= !sclk;
+   end
 end
 
 always @(posedge sclk) begin
-	if (rst) begin
-		state <= IDLE;
-	end
-	case(state)
-		IDLE: begin
-			mosi <= 1'b0;
-			ss <= 1;
-			if(ready_send) begin
-				counter <= 8;
-				ss <= 0;
-				state <= EXGE;
-				data_in_reg <= data_in;
-			end
-		end
-		EXGE: begin
-			if (counter == 0) begin
-				mosi <= 1'b0;
-				ss <= 1;
-				state <= IDLE;
-			end else begin
-				mosi <= data_in_reg[counter - 1]; // send
-				counter <= counter - 1;
-			end
-		end
-	endcase
+   if (rst) begin
+      state <= IDLE;
+   end
+   case(state)
+      IDLE: begin
+         mosi <= 1'b0;
+         ss <= 1;
+         if(ready_send) begin
+            counter <= 8;
+            ss <= 0;
+            state <= EXGE;
+            data_in_reg <= data_in;
+         end
+      end
+      EXGE: begin
+         if (counter == 0) begin
+            mosi <= 1'b0;
+            ss <= 1;
+            state <= IDLE;
+         end else begin
+            mosi <= data_in_reg[counter - 1]; // send
+            counter <= counter - 1;
+         end
+      end
+   endcase
 end
 
 always @(negedge sclk) begin
     if (rst) begin
         data_out <= 0;
     end else if (state == EXGE) begin
-		data_out[counter[2:0]] <= miso; // receive
-	end
+      data_out[counter[2:0]] <= miso; // receive
+   end
 end
 
 endmodule
