14332c3d468dcf813461a915f9f4aff3a332a638
fominok <fominok@hotmail.com>
2016-10-14 14:18:14 +0300

    Fixed Verilog implementation to match SPI mode 0
    SS signal is more finely placed on tact boundaries. SCLK no longer acts
    when no transmission is in progress.

--- verilog/spi.v
+++ verilog/spi.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 
 module spi(
@@ -7,60 +8,61 @@ module spi(
     input ready_send,          // Данные положены в data-in и готовы к отправке
     input rst,
     output reg mosi,           // Master out slave in
-    output reg sclk = 0,       // Тактовый сигнал к рабам
-    output reg ss,             // Slave select
+    output sclk,               // Тактовый сигнал к рабам
+    output ss,                 // Slave select
     output reg[7:0] data_out   // Принятые данные от раба
     );
 
-localparam IDLE = 0;
-localparam EXGE = 1;
-
-reg[7:0] state;
+reg      enabled;
 reg[3:0] counter;
 reg[7:0] data_in_reg;
 reg      clk_tmp = 0;
+reg      done;
+reg      last_tact;
+reg      sclk_p = 0;
+
+assign ss = !(enabled && !done);
+assign sclk = sclk_p && !ss;
 
 always @(posedge clk) begin
-   clk_tmp <= !clk_tmp;
-   if (clk_tmp) begin
-      sclk <= !sclk;
-   end
+    clk_tmp <= !clk_tmp;
+    if (clk_tmp) begin
+        sclk_p <= !sclk_p;
+    end
 end
 
-always @(posedge sclk) begin
+always @(posedge sclk_p) begin
    if (rst) begin
-      state <= IDLE;
-   end
-   case(state)
-      IDLE: begin
-         mosi <= 1'b0;
-         ss <= 1;
-         if(ready_send) begin
-            counter <= 8;
-            ss <= 0;
-            state <= EXGE;
-            data_in_reg <= data_in;
-         end
-      end
-      EXGE: begin
-         if (counter == 0) begin
-            mosi <= 1'b0;
-            ss <= 1;
-            state <= IDLE;
-         end else begin
-            mosi <= data_in_reg[counter - 1]; // send
-            counter <= counter - 1;
-         end
-      end
-   endcase
+       done = 0;
+       counter = 0;
+   end else if (last_tact) begin
+       done = 1;
+   end else if (enabled) begin
+       if (counter == 0) begin
+           counter = 8;
+       end
+       mosi = data_in_reg[counter - 1];
+       counter = counter - 1;
+    end
 end
 
-always @(negedge sclk) begin
+always @(negedge sclk_p) begin
     if (rst) begin
         data_out <= 0;
-    end else if (state == EXGE) begin
-      data_out[counter[2:0]] <= miso; // receive
-   end
+        enabled <= 0;
+        data_in_reg <= 0;
+        last_tact <= 0;
+    end else if (last_tact) begin
+        enabled <= 0;
+        last_tact <= 0;
+    end else if (ready_send && !enabled) begin
+        data_in_reg <= data_in;
+        enabled <= 1;
+    end else if (enabled) begin
+        if (counter == 0)
+            last_tact <= 1;
+        data_out[counter[2:0]] <= miso;
+     end
 end
 
 endmodule
