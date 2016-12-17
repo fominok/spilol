dcc13008aef27dedc67f42e45eb6d15f14fdfcc5
Dmitry Khalansky <rouanth@gmail.com>
2016-12-17 23:47:04 +0300

    Implement clock divisor for SPI
    SCLK can now be configured to be 2k times slower than CLK.

--- verilog/spi.v
+++ verilog/spi.v
@@ -1,17 +1,23 @@
 
 `timescale 1ns / 1ps
 
-module spi(
-    input clk,                 // Тактовый сигнал с процессора
-    input miso,                // Master in slave out
-    input[7:0] data_in,        // Данные для передачи от процессора
-    input ready_send,          // Данные положены в data-in и готовы к отправке
-    input rst,
+module spi #(
+    parameter clk_divisor=8    // SCLK shall be this times slower than CLK;
+                               //     can only be an even number.
+)
+(
+    input clk,                 // Processor clock
+    input rst,                 // Processor reset
+
+    input[7:0] data_in,        // Data to be transferred
+    input ready_send,          // Initialize transfer
+    output reg[7:0] data_out,  // Received data
+    output busy,               // Transaction is in progress
+
+    input      miso,           // Master in slave out
     output reg mosi,           // Master out slave in
-    output sclk,               // Тактовый сигнал к рабам
-    output ss,                 // Slave select
-    output reg[7:0] data_out,  // Принятые данные от раба
-    output busy                // Идёт транзакция по SPI
+    output     sclk,           // SPI clock
+    output     ss              // Slave select
 );
 
 reg      enabled;
@@ -20,37 +26,49 @@ reg[2:0] neg_ctr;
 reg[7:0] data_in_reg;
 reg      done;
 
-assign ss = !enabled;
-assign sclk = !clk || ss;
-assign busy = enabled;
+reg[7:0] ctr;
+reg      sclk_p;
 
-always @(negedge clk) begin
-    if (enabled) begin
-        data_out[pos_ctr] <= miso;
-        if (pos_ctr == 0)
-            done <= 1;
-        neg_ctr <= pos_ctr - 1;
-    end else done <= 0;
-end
+assign ss = !enabled;
+assign sclk = !sclk_p || ss;
+assign busy = enabled;
 
 always @(posedge clk) begin
     if (rst) begin
+        ctr <= 0;
         data_out <= 0;
         enabled <= 0;
         data_in_reg <= 0;
         mosi <= 0;
-    end else if (ready_send && !enabled) begin
-        data_in_reg <= data_in;
-        mosi <= data_in[7];
-        pos_ctr <= 7;
-        enabled <= 1;
-    end else if (enabled) begin
-        if (done) begin
-            enabled <= 0;
+        sclk_p <= 0;
+    end else if (ctr + 1 == clk_divisor >> 1) begin
+        ctr <= 0;
+        if (sclk_p) begin
+            sclk_p <= 0; // Negedge
+            if (enabled) begin
+                data_out[pos_ctr] <= miso;
+                if (pos_ctr == 0)
+                    done <= 1;
+                neg_ctr <= pos_ctr - 1;
+            end else done <= 0;
         end else begin
-            pos_ctr <= neg_ctr;
-            mosi <= data_in[neg_ctr];
+            sclk_p <= 1; // Posedge
+            if (ready_send && !enabled) begin
+                data_in_reg <= data_in;
+                mosi <= data_in[7];
+                pos_ctr <= 7;
+                enabled <= 1;
+            end else if (enabled) begin
+                if (done) begin
+                    enabled <= 0;
+                end else begin
+                    pos_ctr <= neg_ctr;
+                    mosi <= data_in[neg_ctr];
+                end
+            end
         end
+    end else begin
+        ctr <= ctr + 1;
     end
 end
 
