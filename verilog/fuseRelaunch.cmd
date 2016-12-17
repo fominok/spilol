d271701b9a00833f575899e212e0cad5cd64f2c0
Dmitry Khalansky <rouanth@gmail.com>
2016-12-17 17:55:32 +0300

    Remove unneded Xilinx files
    We need to store only *.v, everything else is redundant.

--- verilog/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/yolo/fpga/spilol/test_spi_isim_beh.exe" -prj "/home/yolo/fpga/spilol/test_spi_beh.prj" "work.test_spi" "work.glbl" 
