6d0ff7b07650c02ab59114f151b2b1b5cfa36602
Dmitry Khalansky <rouanth@gmail.com>
2016-12-18 23:08:20 +0300

    Make the amba connector synthesisable
    No longer using negedge -- why would we -- which led to assignment to
    one register in two always blocks.

--- verilog/spi_amba_connector.v
+++ verilog/spi_amba_connector.v
@@ -1,46 +1,37 @@
 `timescale 1ns / 1ps
 
 module spi_amba_connector(
-    input clk,
-    input rst,
-	 
-    input hwrite,
-    input [31:0] hwdata,
-    input [31:0] haddr,
-    input hsel,
-	 output reg [31:0] hrdata,
-    
-    input [7:0] spi_data_out,
-    input spi_busy,
-    output reg [7:0] spi_data_in,
-    output reg spi_ready_send
-    );
-	 
+    input             clk,
+    input             rst,
+
+    input             hwrite,
+    input      [31:0] hwdata,
+    input      [31:0] haddr,
+    input             hsel,
+    output     [31:0] hrdata,
+
+    input      [ 7:0] spi_data_out,
+    input             spi_busy,
+    output reg [ 7:0] spi_data_in,
+    output reg        spi_ready_send
+);
+
 reg [7:0] spi_data_out_reg;
 
-always @(posedge clk) begin
-	if (rst) begin
-		hrdata <= 0;
-		spi_data_in <= 0;
-		spi_ready_send <= 0;
-	end
-	if (hsel) begin
-		if ((haddr == 'h0000) && hwrite && !spi_busy) begin
-			spi_data_in <= hwdata[7:0];
-			spi_ready_send <= 1;
-		end else if ((haddr == 'h0004) && !hwrite) begin
-			hrdata[8] <= spi_busy;
-			hrdata[7:0] <= spi_data_out_reg;
-		end
-	end
-end
+assign hrdata = {{25'b0, spi_busy, spi_busy ? spi_data_out_reg : spi_data_out}};
 
-always @(negedge clk) begin
-	if (!spi_busy) begin
-		spi_data_out_reg <= spi_data_out;
-	end else begin
-		spi_ready_send <= 0;
-	end
+always @(posedge clk) begin
+    if (rst) begin
+        spi_data_in <= 0;
+        spi_ready_send <= 0;
+    end else if (spi_ready_send && spi_busy) begin
+        spi_ready_send <= 0;
+    end else if (!spi_ready_send && !spi_busy) begin
+        if (hsel && haddr == 'h0000 && hwrite) begin
+            spi_data_in <= hwdata[7:0];
+            spi_ready_send <= 1;
+        end
+    end
 end
 
 endmodule
