871e812af4789bb1139b793630c680484cc5b525
Dmitry Khalansky <rouanth@gmail.com>
2016-12-18 12:35:32 +0300

    Linting the verilog code
    Everything is more aligned.

--- verilog/spi.v
+++ verilog/spi.v
@@ -6,61 +6,56 @@ module spi #(
                                //     can only be an even number.
 )
 (
-    input clk,                 // Processor clock
-    input rst,                 // Processor reset
+    input           clk,        // Processor clock
+    input           rst,        // Processor reset
 
-    input[7:0] data_in,        // Data to be transferred
-    input ready_send,          // Initialize transfer
-    output reg[7:0] data_out,  // Received data
-    output busy,               // Transaction is in progress
+    input     [7:0] data_in,    // Data to be transferred
+    output reg[7:0] data_out,   // Received data
+    input           ready_send, // Initialize transfer
+    output          busy,       // Transaction is in progress
 
-    input      miso,           // Master in slave out
-    output reg mosi,           // Master out slave in
-    output     sclk,           // SPI clock
-    output     ss              // Slave select
+    input           miso,       // Master in slave out
+    output reg      mosi,       // Master out slave in
+    output          sclk,       // SPI clock
+    output          ss          // Slave select
 );
 
-reg      enabled;
-reg[2:0] pos_ctr;
-reg[2:0] neg_ctr;
-reg[7:0] data_in_reg;
+reg       enabled;
+reg[ 2:0] pos_ctr;
+reg[ 2:0] neg_ctr;
+reg[ 7:0] data_in_reg;
 
-reg[7:0] ctr;
-reg      sclk_p;
+reg[31:0] ctr;
+reg       sclk_p;
 
-assign ss = !enabled;
+assign ss   = !enabled;
 assign sclk = !sclk_p || ss;
 assign busy = enabled;
 
 always @(posedge clk) begin
     if (rst) begin
-        ctr <= 0;
-        data_out <= 0;
-        enabled <= 0;
-        data_in_reg <= 0;
-        mosi <= 0;
-        sclk_p <= 0;
-        pos_ctr <= 0;
-        neg_ctr <= 0;
+        ctr         <= 0;
+        data_out    <= 0;
+        enabled     <= 0;
     end else if (ready_send && !enabled) begin
         data_in_reg <= data_in;
-        mosi <= data_in[7];
-        pos_ctr <= 7;
-        ctr <= 0;
-        sclk_p <= 1;
+        mosi        <= data_in[7];
+        pos_ctr     <= 7;
+        ctr         <= 0;
+        sclk_p      <= 1;
     end else if (enabled && ctr + 1 == clk_divisor >> 1) begin
         ctr <= 0;
         if (sclk_p) begin
-            sclk_p <= 0; // Negedge
+            sclk_p            <= 0; // Negedge
             data_out[pos_ctr] <= miso;
-            neg_ctr <= pos_ctr - 1;
+            neg_ctr           <= pos_ctr - 1;
         end else begin
-            sclk_p <= 1; // Posedge
+            sclk_p            <= 1; // Posedge
             if (neg_ctr == 7) begin
                 enabled <= 0;
             end else begin
                 pos_ctr <= neg_ctr;
-                mosi <= data_in[neg_ctr];
+                mosi    <= data_in[neg_ctr];
             end
         end
     end else if (enabled) begin
--- verilog/test_spi.v
+++ verilog/test_spi.v
@@ -3,66 +3,59 @@
 module test_spi;
 
 // Inputs
-reg clk;
-reg miso;
-reg[7:0] data_in;
-reg ready_send;
-reg rst;
+reg       clk;
+reg       miso;
+reg[7:0]  data_in;
+reg       ready_send;
+reg       rst;
 
 // Outputs
-wire mosi;
-wire sclk;
-wire ss;
-wire busy;
+wire      mosi;
+wire      sclk;
+wire      ss;
+wire      busy;
 wire[7:0] data_out;
 
 // Instantiate the Unit Under Test (UUT)
 spi uut (
-    .clk(clk),
-    .miso(miso),
-    .data_in(data_in),
-    .ready_send(ready_send),
-    .rst(rst),
-    .mosi(mosi),
-    .sclk(sclk),
-    .ss(ss),
-    .busy(busy),
-    .data_out(data_out)
+    .clk        (clk       ),
+    .miso       (miso      ),
+    .data_in    (data_in   ),
+    .ready_send (ready_send),
+    .rst        (rst       ),
+    .mosi       (mosi      ),
+    .sclk       (sclk      ),
+    .ss         (ss        ),
+    .busy       (busy      ),
+    .data_out   (data_out  )
 );
 
 
 
 initial begin
     // Initialize Inputs
-    clk = 0;
+    clk  = 0;
     miso = 0;
-    rst = 1;
+    rst  = 1;
     // Wait for two clocks for the global reset to finish
     @(posedge clk);
     @(posedge clk);
 
-    rst = 0;
-    data_in = 8'b00010011; // 13
+    rst        = 0;
+    data_in    = 8'b00010011; // 13
     ready_send = 1;
     @(busy == 1);
     ready_send = 0;
     #1;
     // 37 8'b00110111
     miso = 0;
-    @(negedge sclk);
-    miso = 0;
-    @(negedge sclk);
-    miso = 1;
-    @(negedge sclk);
-    miso = 1;
-    @(negedge sclk);
-    miso = 0;
-    @(negedge sclk);
-    miso = 1;
-    @(negedge sclk);
-    miso = 1;
-    @(negedge sclk);
-    miso = 1;
+    @(negedge sclk); miso = 0;
+    @(negedge sclk); miso = 1;
+    @(negedge sclk); miso = 1;
+    @(negedge sclk); miso = 0;
+    @(negedge sclk); miso = 1;
+    @(negedge sclk); miso = 1;
+    @(negedge sclk); miso = 1;
     @(busy == 0);
 
     @(posedge clk);
