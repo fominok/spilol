e68e5f617e450b97664d8ecea084195b8585bf94
Дмитрий Халанский <rouanth@gmail.com>
2016-12-20 18:58:27 +0400

    More robust finite state machine in spi.v
    Various small fixes directed to having as few registers representing state as possible.

--- verilog/spi.v
+++ verilog/spi.v
@@ -2,7 +2,8 @@
 `timescale 1ns / 1ps
 
 module spi #(
-    parameter clk_divisor=8    // SCLK shall be this times slower than CLK;
+    parameter clk_divisor=8    // SCLK shall be this many
+                               //     times slower than CLK;
                                //     can only be an even number.
 )
 (
@@ -15,51 +16,36 @@ module spi #(
     output          busy,       // Transaction is in progress
 
     input           miso,       // Master in slave out
-    output reg      mosi,       // Master out slave in
+    output          mosi,       // Master out slave in
     output          sclk,       // SPI clock
     output          ss          // Slave select
 );
 
-reg       enabled;
-reg[ 2:0] pos_ctr;
-reg[ 2:0] neg_ctr;
+reg[ 3:0] sctr;
 reg[ 7:0] data_in_reg;
 
 reg[31:0] ctr;
-reg       sclk_p;
 
-assign ss   = !enabled;
-assign sclk = !sclk_p || ss;
-assign busy = enabled;
+assign ss   = sctr == 0;
+assign sclk = !sctr[0] || ss;
+assign mosi = data_in_reg[sctr >> 1];
+assign busy = !ss;
 
 always @(posedge clk) begin
     if (rst) begin
+        sctr        <= 0;
         ctr         <= 0;
         data_out    <= 0;
-        enabled     <= 0;
-    end else if (ready_send && !enabled) begin
+    end else if (ready_send && !sctr) begin
         data_in_reg <= data_in;
-        mosi        <= data_in[7];
-        pos_ctr     <= 7;
-        enabled     <= 1;
+        sctr        <= 15;
         ctr         <= 0;
-        sclk_p      <= 1;
-    end else if (enabled && ctr + 1 == clk_divisor >> 1) begin
+    end else if (sctr && ctr + 1 == clk_divisor >> 1) begin
         ctr <= 0;
-        if (sclk_p) begin
-            sclk_p            <= 0; // Negedge
-            data_out[pos_ctr] <= miso;
-            neg_ctr           <= pos_ctr - 1;
-        end else begin
-            sclk_p            <= 1; // Posedge
-            if (neg_ctr == 7) begin
-                enabled <= 0;
-            end else begin
-                pos_ctr <= neg_ctr;
-                mosi    <= data_in[neg_ctr];
-            end
-        end
-    end else if (enabled) begin
+        if (sctr[0])
+            data_out[sctr >> 1] <= miso;
+        sctr <= sctr - 1;
+    end else if (sctr) begin
         ctr <= ctr + 1;
     end
 end
