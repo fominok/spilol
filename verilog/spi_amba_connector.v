25d337f7efcafc1c27593cd823bfdf1ad71780da
Dmitry Khalansky <rouanth@gmail.com>
2016-12-22 20:02:16 +0300

    Fix SPI AHB connector after testing with MIPSFpga
    Our connector used to check the wrong address as well as waiting for too
    long for next transaction on AMBA.

--- verilog/spi_amba_connector.v
+++ verilog/spi_amba_connector.v
@@ -19,7 +19,7 @@ module spi_amba_connector(
 reg [7:0] spi_data_out_reg;
 reg [7:0] spi_data_in_reg;
 
-assign hrdata = {{25'b0, spi_busy, spi_busy ? spi_data_out_reg : spi_data_out}};
+assign hrdata = {{25'b0, phase || spi_busy || spi_ready_send, spi_busy ? spi_data_out_reg : spi_data_out}};
 
 localparam IDLE = 0, DATA = 1;
 
@@ -30,22 +30,22 @@ always @(posedge clk) begin
     if (rst) begin
         spi_data_in <= 0;
         spi_ready_send <= 0;
-		  phase <= 0;
+        phase <= 0;
     end else if (spi_ready_send && spi_busy) begin
         spi_ready_send <= 0;
     end else if (!spi_ready_send && !spi_busy) begin
-		  if (!phase) begin
-		      if (hsel && haddr == 'h0000 && hwrite) phase <= 1;
-		  end else begin
-		      spi_data_in <= spi_data_in_reg;
-				spi_ready_send <= 1;
-				phase <= 0;
-		  end
+        if (!phase) begin
+            if (hsel && haddr[15:0] == 'h0000 && hwrite) phase <= 1;
+        end else begin
+            spi_data_in <= spi_data_in_reg;
+              spi_ready_send <= 1;
+              phase <= 0;
+        end
     end
 end
 
 always @(negedge clk) begin
-	if (phase) spi_data_in_reg <= hwdata[7:0];
+    if (phase) spi_data_in_reg <= hwdata[7:0];
 end
 
 endmodule
